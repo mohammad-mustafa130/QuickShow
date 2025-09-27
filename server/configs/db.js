4b434a64dd7fab75d1ea10974f0fdadcfa4343a4
Mohammad Mustafa <mustafamohd7636@gmail.com>
2025-09-27 19:55:58 +0530

    some changes

--- server/configs/db.js
+++ server/configs/db.js
@@ -3,9 +3,9 @@ import mongoose from "mongoose";
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGODB_URI);
-        console.log("MongoDB Connected.");
+        console.log("Database connected");
     } catch(error){
-        console.error(error);
+        console.log(error.message);
     }
 }
 export default connectDB;
