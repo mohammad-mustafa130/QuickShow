adb1be1ee2f459903e9f4451488fa842c996653e
Mohammad Mustafa <mustafamohd7636@gmail.com>
2025-10-02 19:58:29 +0530

    commit

--- client/src/pages/admin/AddShows.jsx
+++ client/src/pages/admin/AddShows.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { dummyShowsData } from '../../assets/assets';
 import Loading from '../../components/Loading';
 import Title from '../../components/admin/Title';
-import { CheckIcon, StarIcon } from 'lucide-react'
+import { CheckIcon, DeleteIcon, StarIcon } from 'lucide-react'
 import kConverter from '../../components/admin/kConverter';
 
 const AddShows = () => {
@@ -27,7 +27,6 @@ const AddShows = () => {
       const times = prev[date] || [];
       if(!times.includes(time)){
         return {...prev, [date]: [...times, time]};
-
       }
       return prev;
     });
@@ -98,6 +97,29 @@ const AddShows = () => {
           <button onClick={handleDateTimeAdd} className='bg-primary/80 text-white px-3 py-2 text-sm rounded-lg hover:bg-primary cursor-pointer'>Add Time</button>
         </div>
       </div>
+
+      {/* Display Selected Times */}
+      {Object.keys(dateTimeSelection).length > 0 && (
+        <div className='mt-6'>
+          <h2 className='mb-2'>Selected Date-Time</h2>
+          <ul className='space-y-3'>
+            {Object.entries(dateTimeSelection).map(([date, times]) => (
+              <li key={date}>
+                <div className='font-medium'>{date}</div>
+                <div className='flex flex-wrap gap-2 mt-1 text-sm'>
+                  {times.map((time) => (
+                    <div key={time} className='border border-primary px-2 py-1 flex items-center rounded'>
+                      <span>{time}</span>
+                      <DeleteIcon onClick={() => handleRemoveTime(date, time)} width={15} className='ml-2 text-red-500 hover:text-red-700 cursor-pointer'/>
+                    </div> 
+                  ))}
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+      <button className='bg-primary text-white px-8 py-2 mt-6 rounded hover:bg-primary/90 transition-all cursor-pointer'>Add Show</button>
     </>
   ) : <Loading />
 }
